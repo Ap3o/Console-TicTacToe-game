7558f314c15eddf3afe2f785c3dab8a96515dc7d
NeoD <flesakov.nik09@gmail.com>
2020-02-18 23:27:59 +0500

    Добавил псевдо-AI

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+from random import randint
+
 board = [
     [" ", " ", " "],
     [" ", " ", " "],
@@ -8,7 +10,14 @@ MOVES = {1: {"x": 0, "y": 0}, 2: {"x": 0, "y": 1}, 3: {"x": 0, "y": 2},
          4: {"x": 1, "y": 0}, 5: {"x": 1, "y": 1}, 6: {"x": 1, "y": 2},
          7: {"x": 2, "y": 0}, 8: {"x": 2, "y": 1}, 9: {"x": 2, "y": 2},
          }
-
+def render(board=board):
+    str_line = "--------------"
+    print('\n' + str_line)
+    for row in board:
+        for cell in row:
+            symbol = cell
+            print(f'| {symbol} |', end='')
+        print('\n' + str_line)
 
 def empty_cells(board=board):
     cells = []
@@ -21,8 +30,7 @@ def empty_cells(board=board):
     return cells
 
 
-def game_over(board):
-    global player_1, player_2
+def game_over(player_1, player_2, board):
     return is_win(player_1) or is_win(player_2) or len(empty_cells()) == 0
 
 
@@ -55,11 +63,24 @@ def is_valid_move(move, MOVES=MOVES):
     else:
         return False
 
-
-def turn(player, board=board):
-    if game_over(board):
+    
+def ai_turn(ai_comp, player, board=board):
+    if game_over(ai_comp, player, board):
         return
-    print(player)
+    print(f'Ход искусственного интеллекта')
+    while True:
+        move = randint(1, 9)
+        if not is_valid_move(move):
+            move = -1
+        else:
+            set_move(move, ai_comp)
+            break
+    render()
+
+def turn(player, player_2, board=board):
+    if game_over(player, player_2, board):
+        return
+    print(f'Ход игрока "{player}"')
     move = -1
     while move < 1 or move > 9:
         try:
@@ -73,31 +94,36 @@ def turn(player, board=board):
             else:
                 set_move(move, player)
                 break
-    str_line = "--------------"
-    print('\n' + str_line)
-    for row in board:
-        for cell in row:
-            symbol = cell
-            print(f'| {symbol} |', end='')
-        print('\n' + str_line)
+    render()
 
 
 def main():
     global player_1, player_2
     player_1 = ""
     player_2 = ""
-
+    ai_comp = ""
     while player_1 != "X" and player_1 != "0":
         player_1 = input("Choose 'X' or '0': ")
-
-    if player_1 == "X":
-        player_2 = "0"
+    while ai_comp != "Yes" and ai_comp != "No":
+        ai_comp = input("Play with AI? Yes/No: ")
+    render()
+    if ai_comp == "Yes":
+        if player_1 == "X":
+            ai_comp = "0"
+        else:
+            ai_comp = "X"
+        while not game_over(player_1, ai_comp, board):
+            turn(player_1, ai_comp)
+            ai_turn(ai_comp, player_1)
     else:
-        player_2 = "X"
-
-    while not game_over(board):
-        turn(player_1)
-        turn(player_2)
+        if player_1 == "X":
+            player_2 = "0"
+        else:
+            player_2 = "X"
+        
+        while not game_over(player_1, player_2, board):
+            turn(player_1, player_2)
+            turn(player_2, player_1)
 
 
 main()
